098d5416934ec12a59c91f72d1b80395da2bd456
unknown <Esteban Valacco>
2021-08-12 11:18:07 -0300

    Finish Homework 04

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -202,6 +202,7 @@ function todosIguales(arreglo) {
   //Escribe tu código aquí  
 
   for( var i = 0; i < arreglo.length-1; i++){
+    
     if( arreglo[i] !== arreglo[i+1]){
       return false;
     }else{
